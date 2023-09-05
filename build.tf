c5731a51f36b43cb3529980c2a4726cac2a50116
manoj <ankireddy828@gmail.com>
2023-09-05 08:04:36 +0530

    checking

---
+++ build.tf
@@ -0,0 +1,19 @@
+resource "docker_image" "terraform" {
+  name = "000100000/dockercheck"
+  build {
+    context = "."
+  }
+  triggers = {
+    dir_sha1 = sha1(join("", [for f in fileset(path.module, "src/*") : filesha1(f)]))
+  }
+}
+
+resource "docker_container" "kishore" {
+  name  = "checking"
+  image = docker_image.terraform.image_id
+
+  ports {
+    internal = "80"
+    external = "9000"
+  }
+}
